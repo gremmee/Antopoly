a4881e2d2a81f5bd52d07aaddda9df73a8df4b3b
Gremmee <gremmee@gmail.com>
2017-04-01 16:04:39 +0200

    Created an all test suite

---
+++ src/nl/gremmee/antopoly/AllTests.java
@@ -0,0 +1,13 @@
+package nl.gremmee.antopoly;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import nl.gremmee.antopoly.core.CoreTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CoreTests.class })
+public class AllTests {
+
+}
