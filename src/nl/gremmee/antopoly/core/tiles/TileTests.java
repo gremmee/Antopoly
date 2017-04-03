684ed1b3859495f623eb702cdb74099165c3909b
Gremmee <gremmee@gmail.com>
2017-04-03 11:39:15 +0200

    Added JUnits For ChanceTiles and CommunityChestTiles. Fixed call of JailTileTest

--- src/nl/gremmee/antopoly/core/tiles/TileTests.java
+++ src/nl/gremmee/antopoly/core/tiles/TileTests.java
@@ -5,8 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TileListTest.class, FreeParkingTileTest.class, GotoJailTileTest.class, JailTile.class,
-        StartTileTest.class, StationTileTest.class, StreetTileTest.class, TaxTileTest.class, UtilityTileTest.class })
+@SuiteClasses({ TileListTest.class, ChanceTileTest.class, CommunityChestTileTest.class, FreeParkingTileTest.class,
+        GotoJailTileTest.class, JailTileTest.class, StartTileTest.class, StationTileTest.class, StreetTileTest.class,
+        TaxTileTest.class, UtilityTileTest.class })
 public class TileTests {
 
 }
