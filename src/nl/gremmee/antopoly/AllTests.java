9284953a00f96caed95802b11569eafcf3cfca4e
Gremmee <gremmee@gmail.com>
2017-04-03 12:02:24 +0200

    Created Junits Player

--- src/nl/gremmee/antopoly/AllTests.java
+++ src/nl/gremmee/antopoly/AllTests.java
@@ -5,9 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import nl.gremmee.antopoly.core.CoreTests;
+import nl.gremmee.antopoly.players.PlayerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CoreTests.class })
+@SuiteClasses({ CoreTests.class, PlayerTest.class })
 public class AllTests {
 
 }
