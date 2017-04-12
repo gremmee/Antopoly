2917eb07255d240f0dab80b2c5dbf30ab6bcc71b
Gremmee <gremmee@gmail.com>
2017-04-12 15:38:28 +0200

    Optimized Artificial Intelligence List

--- src/nl/gremmee/antopoly/core/lists/ArtificialIntelligenceList.java
+++ src/nl/gremmee/antopoly/core/lists/ArtificialIntelligenceList.java
@@ -8,6 +8,12 @@ public class ArtificialIntelligenceList extends ArrayList<IArtificialIntelligenc
 
     private static final long serialVersionUID = 7932809588117562310L;
 
+    @Override
+    public boolean add(final IArtificialIntelligence aArtificialIntelligence) {
+        System.out.println("Creating ArtificialIntelligence " + aArtificialIntelligence.getName() + "...[OK]");
+        return super.add(aArtificialIntelligence);
+    }
+
     public IArtificialIntelligence getAIByName(final String aName) {
         for (IArtificialIntelligence artificialIntelligence : this) {
             if (artificialIntelligence.getName().equals(aName))
--- src/nl/gremmee/antopoly/initialize/InitializeArtificialIntelligences.java
+++ src/nl/gremmee/antopoly/initialize/InitializeArtificialIntelligences.java
@@ -1,10 +1,10 @@
 package nl.gremmee.antopoly.initialize;
 
 import nl.gremmee.antopoly.core.lists.ArtificialIntelligenceList;
-import nl.gremmee.antopoly.players.ai.IArtificialIntelligence;
 import nl.gremmee.antopoly.players.ai.impl.AIAggressive;
 
 public class InitializeArtificialIntelligences {
+
     private static InitializeArtificialIntelligences instance;
     private ArtificialIntelligenceList artificialIntelligenceList;
 
@@ -19,12 +19,13 @@ public class InitializeArtificialIntelligences {
     }
 
     public ArtificialIntelligenceList initializeArtificialIntelligenceList() {
-        System.out.println("Initializing Artificial Intelligences");
-        artificialIntelligenceList = new ArtificialIntelligenceList();
-        IArtificialIntelligence ai = new AIAggressive();
-        artificialIntelligenceList.add(ai);
-        System.out.println("Creating Artificial Intelligences " + ai + "...[OK]");
+        if (this.artificialIntelligenceList == null) {
+            System.out.println("Initializing Artificial Intelligences");
 
+            this.artificialIntelligenceList = new ArtificialIntelligenceList();
+
+            artificialIntelligenceList.add(new AIAggressive());
+        }
         return artificialIntelligenceList;
     }
 
